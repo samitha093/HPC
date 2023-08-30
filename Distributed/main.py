64f59ee8215563a94d9cdbbd33b15867194d7220
lakshan Pathiraja <lakshanSpathiraja@proton.me>
2023-08-30 06:50:44 +0530

    Update main.py

--- Distributed/main.py
+++ Distributed/main.py
@@ -16,17 +16,21 @@ size = comm.Get_size()
 
 # Create a KMeans instance and fit it to the data
 kmeans = KMeans(n_clusters=K, max_iter=max_iter, comm=comm, file_prefix="kmeans_plots/kmeans_clustering")
-kmeans.fit(DataFile)
+kmeans.fit(DataFile, DataSetSize, False)
 
 # Generate a GIF animation of the clustering process
 from util import generate_gif
 
-# Define input and output paths and file prefix
-input_path = "kmeans_plots"
-file_prefix = "kmeans_clustering"
-output_path = "images"
-output_file = "kmeans_clustering_animate.gif"
-duration = 1000  # Duration between frames in milliseconds
+if rank == 0:
+    # Define input and output paths and file prefix
+    input_path = "kmeans_plots"
+    file_prefix = "kmeans_clustering"
+    output_path = "images"
+    output_file = "kmeans_clustering_animate.gif"
+    duration = 1000  # Duration between frames in milliseconds
 
-# Generate the GIF animation
-generate_gif(path=input_path, file_prefix=file_prefix, output_path=output_path, output_file=output_file, duration=duration) 
+    # Generate the GIF animation
+    try:
+        generate_gif(path=input_path, file_prefix=file_prefix, output_path=output_path, output_file=output_file, duration=duration) 
+    except:
+        print("Error: Could not generate GIF animation")
